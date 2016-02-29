81d32c3e4fdd47f1f107581e567c5da0bad926cf
Juraci Vieira <juraci.vieira@gmail.com>
2016-02-29 14:58:08 -0300

    Serving client side JavaScript

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var app = express();
+"use strict";
 
+let express = require('express');
+let app = express();
+const PORT = 8080;
+
+app.use(express.static(`${__dirname}/public/`));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('pages/index', {name: 'Juraci'});
 });
 
-app.listen(8080, function() {
-    console.log('server listening on 8080');
+app.listen(PORT, function() {
+    console.log(`server listening on ${PORT}`);
 });
